3fe168ce5c4854bfc89ad9095d87abad4dce7b8c
Upasana Kakkar <upasanakakkar@UPASKAKK-RG5546.local>
2020-12-11 15:04:00 +0530

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { AuthGuard } from './guards/auth.guard';
+import { LandingPageComponent } from './core/components/landing-page/landing-page.component';
+import { SignupPageComponent } from './signup/components/signup-page/signup-page.component';
+import { LoginPageComponent } from './login/components/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: SignupPageComponent
+},
+{
+  path: 'login',
+  component: LoginPageComponent,
+},
+{
+  path: 'profile',
+  component: LandingPageComponent,
+  canActivate: [AuthGuard]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageModule } from './login/components/login-page/login-page.module';
+import { SignupPageModule } from './signup/components/signup-page/signup-page.module';
+import { LandingPageModule } from './core/components/landing-page/landing-page.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginPageModule,
+    SignupPageModule,
+    LandingPageModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
